1467200ed6306388828d77eec2254923c04a82c4
Conrad Orta <conradorta@gmail.com>
2020-07-02 16:47:06 -0400

    add-book page end

--- routes.js
+++ routes.js
@@ -1,16 +1,61 @@
 const express = require('express');
+const csrf = require('csurf');
 const db = require('./db/models');
 
 const router = express.Router();
 
-router.get('/', async (req, res, next) => {
+const csrfProtection = csrf({ cookie: true });
+
+const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
+
+
+router.get('/', asyncHandler(async (req, res, next) => {
   try {
     const books = await db.Book.findAll({ order: [['title', 'ASC']] });
-    res.render('index', { title: 'Home', books });
+    res.render('book-list', { title: 'Books', books });
   } catch (err) {
     next(err);
   }
+}));
+
+router.get('/book/add', csrfProtection, (req, res) => {
+  const book = db.Book.build();
+  res.render('book-add', {
+    title: 'Add Book',
+    book,
+    csrfToken: req.csrfToken(),
+  });
 });
+
+router.post('/book/add', csrfProtection, asyncHandler(async (req, res) => {
+  const {
+    title,
+    author,
+    releaseDate,
+    pageCount,
+    publisher,
+  } = req.body;
+
+  const book = db.Book.build({
+    title,
+    author,
+    releaseDate,
+    pageCount,
+    publisher,
+  });
+
+  try {
+    await book.save();
+    res.redirect('/');
+  } catch (err) {
+    res.render('book-add', {
+      title: 'Add Book',
+      book,
+      error: err,
+      csrfToken: req.csrfToken(),
+    });
+  }
+}));
 
 // router.get('/', (req, res) => {
 //   throw new Error('This is a test error!');

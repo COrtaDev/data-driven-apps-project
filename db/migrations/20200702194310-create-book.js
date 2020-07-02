bfe1b78a4a7b9f967201c32fd10a9060f26541f6
Conrad Orta <conradorta@gmail.com>
2020-07-02 16:11:13 -0400

    seeded and querying

---
+++ db/migrations/20200702194310-create-book.js
@@ -0,0 +1,83 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Books', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      author: {
+        type: Sequelize.STRING(100),
+        allowNull: false
+      },
+      releaseDate: {
+        type: Sequelize.DATEONLY,
+        allowNull: false
+      },
+      pageCount: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      publisher: {
+        type: Sequelize.STRING(100),
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Books');
+  }
+};
+// 'use strict';
+// module.exports = {
+//   up: (queryInterface, Sequelize) => {
+//     return queryInterface.createTable('Books', {
+//       id: {
+//         allowNull: false,
+//         autoIncrement: true,
+//         primaryKey: true,
+//         type: Sequelize.INTEGER
+//       },
+//       title: {
+//         type: Sequelize.STRING
+//       },
+//       author: {
+//         type: Sequelize.STRING
+//       },
+//       releaseDate: {
+//         type: Sequelize.DATEONLY
+//       },
+//       pageCount: {
+//         type: Sequelize.INTEGER
+//       },
+//       publisher: {
+//         type: Sequelize.STRING
+//       },
+//       createdAt: {
+//         allowNull: false,
+//         type: Sequelize.DATE
+//       },
+//       updatedAt: {
+//         allowNull: false,
+//         type: Sequelize.DATE
+//       }
+//     });
+//   },
+//   down: (queryInterface, Sequelize) => {
+//     return queryInterface.dropTable('Books');
+//   }
+// };
